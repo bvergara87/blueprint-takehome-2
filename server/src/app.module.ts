d9f845df0887896e91e656493be90d6fa6e3dd77
Bryant Vergara <bryant.vergara@gmail.com>
2025-03-08 11:04:00 -0800

    added fly

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { AssessmentController } from "./controllers/assessment.controller";
 import { AssessmentService } from "./services/assessment.service";
 import { SupabaseService } from "./services/supabase.service";
+import { AppController } from "./controllers/app.controller";
 
 @Module({
   imports: [],
-  controllers: [AssessmentController],
+  controllers: [AssessmentController, AppController],
   providers: [AssessmentService, SupabaseService],
 })
 export class AppModule {}
